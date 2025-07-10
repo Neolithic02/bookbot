598c7fded8f975fc210671dccedbab687bce7a4a
Neolithic02 <Neolithic@frontier.com>
2025-07-09 21:32:37 -0500

    report print update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_book_wordcount  # Import the function to count words from stats module
 from stats import get_book_lettercount  # Import the function to count letters from stats module
 from stats import letter_count  # Import the letter count function from stats module
+from stats import sort_letter_count  # Import the function to sort letter counts from stats module
 # Import necessary functions from stats module
 
 
@@ -10,12 +11,16 @@ def get_book_text(filepath): # Function to read the book text from a file
         text = f.read() # Read the entire content of the file
     return text # Return the text read from the file
 
+
 def main():
     filepath = 'books/frankenstein.txt'  # Path to the book file
     word_count = get_book_wordcount(filepath)  # Count the words in the book text
-    print(f'{word_count} words found in the document')  # Display the word count
-    testx=get_book_lettercount(filepath)  # Count the letters in the book text and display the count
-    print(testx)
-
+    print(f'Found {word_count} total words')  # Display the word count
+    lettercountoutput=get_book_lettercount(filepath)  # Count the letters in the book text and display the count
+    sorted_letter_count = sort_letter_count(lettercountoutput)  # Sort the letter counts
+    for letter, count in sorted_letter_count:
+        if letter.isalpha():  # Check if the character is a letter
+            print(f'{letter}: {count}') # Print the letter and its count
+    
 # This is the main entry point of the program
 main()  # Call the main function to execute the program
